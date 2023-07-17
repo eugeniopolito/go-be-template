ed356a1faf6e0d3080f06d504449c8bd8f169228
Eugenio Polito <e.polito@gmail.com>
2023-07-17 09:32:13 +0200

    refactoring

--- worker/distributor.go
+++ worker/distributor.go
@@ -2,8 +2,11 @@ package worker
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/hibiken/asynq"
+	log "github.com/rs/zerolog/log"
 )
 
 type TaskDistributor interface {
@@ -23,4 +26,31 @@ func NewRedisTaskDistributor(redisOpt asynq.RedisClientOpt) TaskDistributor {
 	return &RedisTaskDistributor{
 		client: client,
 	}
+}
+
+type PayloadSendVerifyEmail struct {
+	Username string `json:"username"`
+}
+
+const TaskSendVerifyEmail = "task:send_verify_email"
+
+func (distributor *RedisTaskDistributor) DistributeTaskSendVerifyEmail(
+	ctx context.Context,
+	payload *PayloadSendVerifyEmail,
+	opts ...asynq.Option,
+) error {
+	jsonPaylod, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal task payload: %w", err)
+	}
+	task := asynq.NewTask(TaskSendVerifyEmail, jsonPaylod, opts...)
+	info, err := distributor.client.EnqueueContext(ctx, task)
+	if err != nil {
+		return fmt.Errorf("failed to enque task: %w", err)
+	}
+
+	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
+		Str("queue", info.Queue).Int("max_retry", info.MaxRetry).Msg("enqueued task")
+
+	return nil
 }
--- worker/processor.go
+++ worker/processor.go
@@ -2,6 +2,9 @@ package worker
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
 
 	db "github.com/eugeniopolito/gobetemplate/db/sqlc"
 	"github.com/eugeniopolito/gobetemplate/mail"
@@ -50,4 +53,45 @@ func (processor *RedisTaskProcessor) Start() error {
 	mux := asynq.NewServeMux()
 	mux.HandleFunc(TaskSendVerifyEmail, processor.ProcessSendVerifyEmail)
 	return processor.server.Start(mux)
+}
+
+func (processor *RedisTaskProcessor) ProcessSendVerifyEmail(ctx context.Context, task *asynq.Task) error {
+	var payload PayloadSendVerifyEmail
+	if err := json.Unmarshal(task.Payload(), &payload); err != nil {
+		return fmt.Errorf("failed to unmarshall payload: %w", asynq.SkipRetry)
+	}
+
+	user, err := processor.store.GetUser(ctx, payload.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("user not found: %w", asynq.SkipRetry)
+		}
+		return fmt.Errorf("failed to get user: %w", asynq.SkipRetry)
+	}
+
+	arg := db.CreateVerifyEmailParams{
+		Username:   payload.Username,
+		Email:      user.Email,
+		SecretCode: util.RandomString(32),
+	}
+
+	verifyEmail, err := processor.store.CreateVerifyEmail(ctx, arg)
+
+	if err != nil {
+		return fmt.Errorf("failed to create verify email: %w", asynq.SkipRetry)
+	}
+
+	verifyURL := fmt.Sprintf(processor.config.VerifyEmailAddress+"?email_id=%d&secret_code=%s", verifyEmail.ID, verifyEmail.SecretCode)
+	subject := processor.config.VerifyEmailSubject
+	content := fmt.Sprintf(processor.config.VerifyEmailBody, user.Name, user.Surname, verifyURL)
+
+	to := []string{user.Email}
+	err = processor.mailer.SendEmail(subject, content, to, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to send verify email: %w", asynq.SkipRetry)
+	}
+
+	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
+		Str("email", user.Email).Msg("processed task")
+	return nil
 }
